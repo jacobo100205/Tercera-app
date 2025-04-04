6f485e95e2a2a0056d7f46b50aab48fa4e78703e
Sala 203 <sala203@ujaverianas.loc>
2025-04-04 15:02:16 -0500

    second commit

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ st.title("Esto es una app")
 year = st.slelectbox("Seleccione un año",[2024,2023,2022])
 
 if year == 2024:
-    df_m.read_parquet("hombres24.parquet")
-    df_f.read_parquet_parquet("mujeres24.parquet")
+    df_m = gpd.read_parquet("hombres24.parquet")
+    df_f = gpd.read_parquet("mujeres24.parquet")
 elif year == 2023:
-    df_m.read_parquet("hombres23.parquet")
-    df_f.read_parquet("mujeres23.parquet")
+    df_m = gpd.read_parquet("hombres23.parquet")
+    df_f = gpd.read_parquet("mujeres23.parquet")
 else year == 2022:
-    df_m.read_parquet("hombres22.parquet")
-    df_f.read_parquet("mujeres22.parquet")
+    df_m  = gpd.read_parquet("hombres22.parquet")
+    df_f = gpd.read_parquet("mujeres22.parquet")
 
 
 df_m = pgd.read_file('hombres.geojson')
